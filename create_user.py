8748e6d4a007e152a3d90180b11925bb500fe666
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2023-10-07 06:12:04 +0800

    added dynamic param reading

--- create_user.py
+++ create_user.py
@@ -4,11 +4,12 @@ import os
 
 from passgen import generate_password
 
-username = "test_create_user"
-employeid = "1234556"
-target_email = "johnvincentagbayani@deltek.com"
-snow_case = "sctask123456"
+source_user = os.environ["Source User"]
 
+username = os.environ["Username"]
+user_email = os.environ["Email"]
+employeid = os.environ["Employee ID"]
+snow_case = os.environ["ServiceNow Case"]
 user_pwd = generate_password()
 
 env_file = open("temp_env.txt")
@@ -17,6 +18,8 @@ target_environment = target_environment.replace("\n","")
 
 emailref_file = open("email_reference.json")
 emailref_json = json.loads(emailref_file.read())
+
+print(username, user_email, employeid, snow_case, user_pwd, target_environment)
 
 def check_user_existence(username):
 	iam_client = boto3.client('iam')
@@ -29,23 +32,23 @@ def check_user_existence(username):
 
 	return response   
 
-#main execution
-iam_client = boto3.client('iam')
+# #main execution
+# iam_client = boto3.client('iam')
 
-user_exists = check_user_existence(username)
+# user_exists = check_user_existence(username)
 
-if user_exists:
-	print(f'ERROR: {username} already exists in {target_environment}, aborting creation')
-else:
-	iam_create_response = iam_client.create_user(UserName=username,Tags=[{'Key': 'email','Value': target_email},{'Key': 'employeeID','Value': employeeid},{'Key': 'snowcase','Value': snow_case}])
-	print(iam_create_response)
+# if user_exists:
+# 	print(f'ERROR: {username} already exists in {target_environment}, aborting creation')
+# else:
+# 	iam_create_response = iam_client.create_user(UserName=username,Tags=[{'Key': 'email','Value': target_email},{'Key': 'employeeID','Value': employeeid},{'Key': 'snowcase','Value': snow_case}])
+# 	print(iam_create_response)
 
-	iam_profile_response = iam_client.create_login_profile(UserName=username,Password=user_pwd,PasswordResetRequired=True)
-	print(iam_profile_response)
+# 	iam_profile_response = iam_client.create_login_profile(UserName=username,Password=user_pwd,PasswordResetRequired=True)
+# 	print(iam_profile_response)
 
-	setup_group = emailref_json[target_environment]['setup']
-	group_add_response = iam_client.add_user_to_group(GroupName=setup_group, UserName=username)
-	print(group_add_response)
+# 	setup_group = emailref_json[target_environment]['setup']
+# 	group_add_response = iam_client.add_user_to_group(GroupName=setup_group, UserName=username)
+# 	print(group_add_response)
 
-	if iam_create_response and iam_profile_response and group_add_response:
-		print(f'User account {username} has been created in {target_environment} and has been added into {setup_group}')
+# 	if iam_create_response and iam_profile_response and group_add_response:
+# 		print(f'User account {username} has been created in {target_environment} and has been added into {setup_group}')
