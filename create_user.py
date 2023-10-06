3f4d6091a8ca7ac90fe0e9cb4eeed6c070270941
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2023-10-07 06:16:44 +0800

    enabled main execution step

--- create_user.py
+++ create_user.py
@@ -18,8 +18,6 @@ target_environment = target_environment.replace("\n","")
 emailref_file = open("email_reference.json")
 emailref_json = json.loads(emailref_file.read())
 
-print(username, user_email, employeid, snow_case, user_pwd, target_environment)
-
 def check_user_existence(username):
 	iam_client = boto3.client('iam')
 	
@@ -31,23 +29,23 @@ def check_user_existence(username):
 
 	return response   
 
-# #main execution
-# iam_client = boto3.client('iam')
+#main execution
+iam_client = boto3.client('iam')
 
-# user_exists = check_user_existence(username)
+user_exists = check_user_existence(username)
 
-# if user_exists:
-# 	print(f'ERROR: {username} already exists in {target_environment}, aborting creation')
-# else:
-# 	iam_create_response = iam_client.create_user(UserName=username,Tags=[{'Key': 'email','Value': target_email},{'Key': 'employeeID','Value': employeeid},{'Key': 'snowcase','Value': snow_case}])
-# 	print(iam_create_response)
+if user_exists:
+	print(f'ERROR: {username} already exists in {target_environment}, aborting creation')
+else:
+	iam_create_response = iam_client.create_user(UserName=username,Tags=[{'Key': 'email','Value': target_email},{'Key': 'employeeID','Value': employeeid},{'Key': 'snowcase','Value': snow_case}])
+	print(iam_create_response)
 
-# 	iam_profile_response = iam_client.create_login_profile(UserName=username,Password=user_pwd,PasswordResetRequired=True)
-# 	print(iam_profile_response)
+	iam_profile_response = iam_client.create_login_profile(UserName=username,Password=user_pwd,PasswordResetRequired=True)
+	print(iam_profile_response)
 
-# 	setup_group = emailref_json[target_environment]['setup']
-# 	group_add_response = iam_client.add_user_to_group(GroupName=setup_group, UserName=username)
-# 	print(group_add_response)
+	setup_group = emailref_json[target_environment]['setup']
+	group_add_response = iam_client.add_user_to_group(GroupName=setup_group, UserName=username)
+	print(group_add_response)
 
-# 	if iam_create_response and iam_profile_response and group_add_response:
-# 		print(f'User account {username} has been created in {target_environment} and has been added into {setup_group}')
+	if iam_create_response and iam_profile_response and group_add_response:
+		print(f'User account {username} has been created in {target_environment} and has been added into {setup_group}')
