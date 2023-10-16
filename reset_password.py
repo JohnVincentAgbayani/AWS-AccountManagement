c9688478de0467310ec00b129106b1470ce97d18
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2023-10-17 01:57:39 +0800

    modified to accomodate jenkins pipeline extra line breaks

--- reset_password.py
+++ reset_password.py
@@ -8,10 +8,12 @@ from passgen import generate_password
 
 env_file = open("temp_env.txt")
 target_env = env_file.read()
+target_env = target_env.replace("\n","")
 
 user_file = open("temp_user.txt")
 target_user = user_file.read()
 target_user = target_user.strip()
+target_user = target_user.replace("\n","")
 
 iam_client = boto3.client('iam')
 response = ""
